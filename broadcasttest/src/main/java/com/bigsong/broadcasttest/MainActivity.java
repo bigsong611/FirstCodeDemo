be76b380ad571b9cdcd4560fe1c4a5f6e624a039
73739622@qq.com <73739622@qq.com>
2015-12-18 13:59:56 +0800

    增加broadcastDemo

---
+++ broadcasttest/src/main/java/com/bigsong/broadcasttest/MainActivity.java
@@ -0,0 +1,52 @@
+package com.bigsong.broadcasttest;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity {
+
+    private IntentFilter intentFilter;
+
+    private NetworkChangeReceiver networkChangeReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        intentFilter = new IntentFilter();
+        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+        networkChangeReceiver = new NetworkChangeReceiver();
+        registerReceiver(networkChangeReceiver,intentFilter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(networkChangeReceiver);
+    }
+
+    class NetworkChangeReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            //Toast.makeText(context, "net work changes", Toast.LENGTH_SHORT).show();
+
+            ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+            if (networkInfo != null && networkInfo.isAvailable()){
+                Toast.makeText(context,"network is avaliable",Toast.LENGTH_SHORT).show();
+            }else {
+                Toast.makeText(context,"network is unavaliable",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+}
